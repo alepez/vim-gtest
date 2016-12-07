alepez/vim-gtest

commit 1086fc742e60fd78be90446de4fb29393ef5356b

Strip whitespace from test name.
I don't know regexes very well, so it maybe be wrong, but it works for me.
The previous code didn't parse this line well:
TEST( TestCase, TestName )
It produced " TestCase.TestName " instead of "TestCase.TestName".

autoload/gtest.vim
@@ -60,7 +60,8 @@ function! s:GetTestNameFromFull(full)
 endfunction
 
 function! s:GetTestFullFromLine(line)
-  return substitute(a:line, '^TEST.*(\(.*\), *\(.*\)).*$', '\1.\2', '')
+  let l:result = substitute(a:line, '^TEST\s*(\s*\(\S\{-1,}\),\s*\(\S\{-1,}\)\s*).*$', '\1.\2', '')
+  return l:result 
 endfunction
 
 function! gtest#ListTestCases(arg, line, pos)

commit 42985a0560c40996c04f31b4feebc2d45de04ade

Correctly match TEST_F, TEST_P and similar constructs.
This was broken by 1086fc742e60fd78be90446de4fb29393ef5356b.

autoload/gtest.vim
@@ -64,7 +64,7 @@ function! s:GetTestNameFromFull(full)
 endfunction
 
 function! s:GetTestFullFromLine(line)
-  return substitute(a:line, '^TEST\s*(\s*\(\S\{-1,}\),\s*\(\S\{-1,}\)\s*).*$', '\1.\2', '')
+  return substitute(a:line, '^TEST.*(\s*\(\S\{-1,}\),\s*\(\S\{-1,}\)\s*).*$', '\1.\2', '')
 endfunction
 
 function! gtest#ListTestCases(arg, line, pos)